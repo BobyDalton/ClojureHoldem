9626b49f177c1d93ab4009b8d18dd71b5ca4e651
Hannes Steffenhagen <Hannes.Steffenhagen@uni-rostock.de>
2012-10-03 14:40:31 +0200

    Initial Leiningen setup

---
+++ holdem/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to holdem
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ holdem/test/holdem/core_test.clj
@@ -0,0 +1,7 @@
+(ns holdem.core-test
+  (:use clojure.test
+        holdem.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
